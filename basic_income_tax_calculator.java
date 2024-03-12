b6505643fe98c4da4bf912c2fcbb5b236d9f0e73
Arpit-Jain-19 <157736601+Arpit-Jain-19@users.noreply.github.com>
2024-03-13 00:51:56 +0530

    Add files via upload

---
+++ basic_income_tax_calculator.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class basic_income_tax_calculator {
+    public static void main( String args[]) {
+        System.out.println("Income should be positive ");
+        System.out.println("Amount in Lakhs");
+        Scanner sc = new Scanner (System.in);
+        int income = sc.nextInt();
+
+        if(income<=500000){
+            System.out.println("you are not eligible to pay direct tax");
+        }
+        else if(income>=500000 && income<=1000000){
+            int tax = (int)(income*(0.2));
+            System.out.println("your tax ammount is: "+tax);
+        }
+        else{
+            int tax = (int)(income*(0.3));
+            System.out.println("your tax ammount is :" +tax);
+        }
+    }    
+}    
