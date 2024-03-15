60501da3bfb14777a6299671fe7e71b0533d6d26
Arpit-Jain-19 <157736601+Arpit-Jain-19@users.noreply.github.com>
2024-03-15 23:45:41 +0530

    Create counting using while.java

---
+++ counting using while.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class counting {
+    public static void main( String args[]) {
+        int counter = 1;
+        while (counter<=10){
+            System.out.println(counter);
+            counter++;
+        }
+    }
+}
