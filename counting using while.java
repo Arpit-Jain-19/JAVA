e29c962d9adc3f73794e87f732e2fbacd635c85b
Arpit-Jain-19 <157736601+Arpit-Jain-19@users.noreply.github.com>
2024-03-17 20:41:42 +0530

    Create counting using while.java

--- counting using while.java
+++ counting using while.java
@@ -1,7 +1,11 @@
+import java.util.*;
 public class counting {
     public static void main( String args[]) {
+        System.out.print("enter the last number");
+        Scanner sc= new Scanner(System.in);
         int counter = 1;
-        while (counter<=10){
+        int n = sc.nextInt();
+        while (counter<=n){
             System.out.println(counter);
             counter++;
         }
