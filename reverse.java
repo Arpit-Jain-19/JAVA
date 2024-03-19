47032cd42b5e91c888a32038539666768a5a7f07
Arpit-Jain-19 <157736601+Arpit-Jain-19@users.noreply.github.com>
2024-03-19 23:58:46 +0530

    Create reverse.java

---
+++ reverse.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class reverse {
+    public static void main( String args[]) {
+        int n= 123572;
+        while(n>0){
+            int lastdigit=n%10;
+            System.out.println(last digit);
+            n=n/10;
+        }
+        System.out.println();
+    }
+}
