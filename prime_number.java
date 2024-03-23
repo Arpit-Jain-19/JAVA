de4c134e1c3d9f3fcf073bb0a4b04c276bc4d932
Arpit-Jain-19 <157736601+Arpit-Jain-19@users.noreply.github.com>
2024-03-23 16:43:51 +0530

    Create prime_number.java

---
+++ prime_number.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class prime_number
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number :-");
+        int n = sc.nextInt();
+
+        if (n==2){
+            System.out.println("Number is prime");
+        }
+        else {
+            boolean isPrime = true;
+            for ( int i=2 ; i<= Math.sqrt(n); i++){
+                if(n%i==0){
+                    isPrime = false;
+                }
+            }
+        if(isPrime= true){
+                    System.out.println("number is prime");
+                }
+        else{
+                    System.out.println("number is NOT prime");
+                }
+        }
+    }
+}
