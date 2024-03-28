d4c12c10af3a064b58c66157c113c0adadf683cb
Arpit-Jain-19 <157736601+Arpit-Jain-19@users.noreply.github.com>
2024-03-28 23:57:47 +0530

    Create pelindrome_number_check.java

---
+++ pelindrome_number_check.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class pelindrome_number_check
+{
+    public static void main{ String args[]}
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any nymber:");
+        int input= sc.nextInt();
+
+        if(pelindrome(input))
+        {
+            System.out.println("the number"+ input + "is pelindrome");
+        }
+        else
+        {
+            system.out.println("the number "+ input + " is NOT pelindrome");
+        }
+    }
+    public static void pelindrome( int n)
+    {
+        int input = temp
+        int rev = 0 ;
+        while(n!=0)
+        {
+            rem = n%10;
+            
+        }
+
+    }
+}
