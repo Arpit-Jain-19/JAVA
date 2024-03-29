d01f6b6361a23f8746674b6e713d5f73b370caaf
Arpit-Jain-19 <157736601+Arpit-Jain-19@users.noreply.github.com>
2024-03-30 04:09:54 +0530

    Add files via upload

---
+++ solid_rombus.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class solid_rombus
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any nymber:");
+        int n= sc.nextInt();
+
+        for(int i=1; i<=n; i++)
+        {
+            for( int j=1; j<=(n-i); j++)
+            {
+                System.out.print(" ");
+            }
+            for( int j=1; j<=n; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
